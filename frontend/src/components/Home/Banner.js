4f4dad69d4a0d36ce65d187cf0c85961dba97c20
Joshua C Alpuerto <joshuacalpuerto@gmail.com>
2022-07-24 12:20:14 +0300

    Allow users to search their need

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,13 +1,15 @@
 import React from "react";
 import logo from "../../imgs/logo.png";
+import './home.css'
 
 const Banner = () => {
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div>
+        <div >
           <span id="get-part">A place to get</span>
+          <input id="search-box" placeholder="What is it that you truly desire?" type="input" className="w-50 mx-2 rounded"/>
           <span> the cool stuff.</span>
         </div>
       </div>
